b7c6b4dad9cc10138cbbe537751e451861dd3a18
Steve Morris <baramorf.com>
2023-03-02 19:59:07 +0000

    Card Deck Class created and integrated

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -57,6 +57,24 @@ class CardsController < ApplicationController
     end
   end
 
+  def test
+    vdeck = CardDeck.new
+    @vdeck = vdeck.shuffle!
+    @dealer = User.new
+    @player = User.new
+    # Deals hand to Dealer
+    vTmp = @vdeck.draw!
+    @dealer.add(Card.new(vTmp.suit, vTmp.rank))
+    vTmp = @vdeck.draw!
+    @dealer.add(Card.new(vTmp.suit, vTmp.rank))
+    
+    # Deals hand to Player
+    vTmp = @vdeck.draw!
+    @player.add(Card.new(vTmp.suit, vTmp.rank))
+    vTmp = @vdeck.draw!
+    @player.add(Card.new(vTmp.suit, vTmp.rank))
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_card
