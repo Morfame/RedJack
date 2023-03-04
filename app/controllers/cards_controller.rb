dd6b1a4fa24c40a738316cb527e6e5315e945c65
Steve Morris <baramorf.com>
2023-03-04 12:11:24 +0000

    New card deck

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -57,24 +57,10 @@ class CardsController < ApplicationController
     end
   end
 
-  def test
-    vdeck = CardDeck.new
-    @vdeck = vdeck.shuffle!
-    @dealer = User.new
-    @player = User.new
-    # Deals hand to Dealer
-    vTmp = @vdeck.draw!
-    @dealer.add(Card.new(vTmp.suit, vTmp.rank))
-    vTmp = @vdeck.draw!
-    @dealer.add(Card.new(vTmp.suit, vTmp.rank))
-    
-    # Deals hand to Player
-    vTmp = @vdeck.draw!
-    @player.add(Card.new(vTmp.suit, vTmp.rank))
-    vTmp = @vdeck.draw!
-    @player.add(Card.new(vTmp.suit, vTmp.rank))
+  def shoe
+    @vdeck = PlayCard::Deck.new(4)
   end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_card
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,98 +1,20 @@
 class Card < ApplicationRecord
 end
-class CardDeck
-  def initialize(cards = build_cards)
-    @cards = cards
-  end
 
-  def each(&block)
-    @cards.each(&block)
-  end
+Class PlayCard
+  Suits = %w(♠︎ ♣︎ ♥︎ ♦︎)
+  Ranks = %w(2 3 4 5 6 7 8 9 10 J Q K A)
 
-  def size
-    @cards.size
-  end
+  Class Deck
+    attr_reader :cards
 
-  def shuffle!
-    @cards.shuffle!
-    self
-  end
-
-  def draw!
-    @cards.shift
-  end
-
-  def cut!(num_of_cards)
-    [
-      CardDeck.new(@cards[0, num_of_cards]),
-      CardDeck.new(@cards[num_of_cards, @cards.size])
-    ]
-  end
-
-  def +(other_deck)
-    cards_from_other = []
-    other_deck.each do |card|
-      cards_from_other << card
-    end
-
-    CardDeck.new(@cards + cards_from_other)
-  end
-
-  def ==(other_deck)
-    cards_from_other_deck = []
-    other_deck.each do |card|
-      cards_from_other_deck << card
-    end
-
-    @cards == cards_from_other_deck
-  end
-
-  private
-
-  def build_cards
-    [].tap do |cards|
-      %w(♠︎ ♣︎ ♥︎ ♦︎).each do |suit|
-        %w(A 2 3 4 5 6 7 8 9 10 J Q K).each do |rank|
-          cards << Card.new(suit, rank)
+    def def initialize(n=1)
+      @cards=[]
+      Suits.cycle(n) do |s|
+        Ranks.cycle(1) do |r|
+          @cards << "#(r) of #(s)"          
         end
       end
-      #2.times { cards << Card.new("*", "Joker") }
     end
   end
 end
-
-class Card
-  attr_reader :suit, :rank
-
-  def initialize(suit, rank)
-    @suit = suit
-    @rank = rank
-  end
-
-  def ==(other)
-    suit == other.suit && rank == other.rank
-  end
-end
-
-
-
-class Card
-  attr_accessor :rank, :suit
-
-  def initialize(r, s)
-    @rank, @suit = r, s
-  end
-end
-
-class User
-  attr_accessor :cards, :suit
-
-  def initialize
-    @cards =  Array.new
-  end
-
-  def add(b)
-    @cards << b
-  end
-end
-
